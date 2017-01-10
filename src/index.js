099b7e0679a74535a155429a8eb62e9668a5383f
Archie Lee <archielee@appier.com>
2017-01-10 18:43:23 +0800

    Initial project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* global document */
+import App from './App';
+
+const app = new App({
+  target: document.querySelector('#root')
+});
+
---
+++ webpack.config.babel.js
@@ -0,0 +1,30 @@
+
+export default {
+  entry: {
+    'index': [ './src/index.js' ]
+  },
+  resolve: {
+    extensions: [ '.js', '.html' ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: 'svelte-loader'
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+};
+
