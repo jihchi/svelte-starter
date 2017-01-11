0a9cb7d522de2412227ff48c1d53d50829b0d7fc
Archie Lee <archielee@appier.com>
2017-01-11 17:30:04 +0800

    Add support for css

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 export default {
   entry: {
@@ -23,6 +24,28 @@ export default {
         test: /\.html$/,
         exclude: /node_modules/,
         use: 'svelte-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          'postcss-loader',
+        ],
+        options: {
+          plugins: function plugins() {
+            return [
+              autoprefixer({
+                browsers: [
+                  '>1%',
+                  'last 4 versions',
+                  'Firefox ESR',
+                  'not ie < 9',
+                ],
+              }),
+            ];
+          },
+        },
       },
     ],
   },
